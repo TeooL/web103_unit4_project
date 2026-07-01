d8c7a7d964d15c95a8ce0a363f301106a806ec9b
TeooL <teoliang11204@gmail.com>
2026-06-30 22:18:05 -0400

    Fixed backend operations not working

--- server/config/database.js
+++ server/config/database.js
@@ -1,4 +1,11 @@
+import dotenv from 'dotenv'
 import pg from 'pg'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const currentDirectory = path.dirname(fileURLToPath(import.meta.url))
+
+dotenv.config({ path: path.resolve(currentDirectory, '..', '.env') })
 
 const config = {
     user: process.env.PGUSER,
